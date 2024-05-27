04d9dc012941aa1c61c52e8bb41526c74fac457d
Karma Gyelmo <02230315.cst@rub.edu.bt>
2024-05-27 21:34:49 +0530

    kg

---
+++ logical.py
@@ -0,0 +1,23 @@
+# Example 1
+x = 5
+print(not(x > 3 and x<10)) #True because both conditions are True
+
+# Example 2
+y = 12
+print(not(y>1 and y%5==0)) #false because both the condition is False
+
+# Example 1
+x=5
+print(x<3 or x>10) #False because both the conditions are Fa;se
+
+# Example 2
+y=12
+print(y<10 or y%2 ==0) #True because the second condition is True
+
+#Examplw 1
+x=5
+print(not(x>3 and x<10)) #False because the condition inside the not is true
+
+#Example 2
+y=12
+print(not(y>10 and y%5 ==0)) #True because the condition inside the not is False
