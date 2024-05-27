e5a0e2e29f5d1253454bb5bc2deb59ef3935e860
Karma Gyelmo <02230315.cst@rub.edu.bt>
2024-05-27 21:48:13 +0530

    k

---
+++ Variable.py
@@ -0,0 +1,16 @@
+name = input("Karma Gyelmo")
+num1 = float(input("Enter any number:"))
+num2 = float(input(" Enter any number:"))
+
+sum = num1+num2
+print("Addition of num1 and num2:",sum)
+
+Difference = num1-num2
+print("Subtraction of num1 and num2:",Difference)
+
+multiplication = num1*num2
+print("Multiplication of num1 and num2:",multiplication)
+
+Division = num1/num2
+print("Division of num1 and num2:", Division)
+print("Thank you for using the code:",name)
