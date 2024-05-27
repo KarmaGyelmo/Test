3746f0559bef653746591da98e5a30be80e2ac3c
Karma Gyelmo <02230315.cst@rub.edu.bt>
2024-05-27 21:55:24 +0530

    u

---
+++ Ticket.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+is_student = input("Are you a student? (yes/no):")
+
+if age <=12:
+   print("you are eligible for discount")
+
+elif (13 <= age <=18 and is_student == "yes:")
+    
