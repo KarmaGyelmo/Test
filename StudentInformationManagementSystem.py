3418ebc335cf7a84c82fe10da4b5fb7bca2a58f1
Karma Gyelmo <02230315.cst@rub.edu.bt>
2024-05-27 21:49:15 +0530

    l

---
+++ StudentInformationManagementSystem.py
@@ -0,0 +1,8 @@
+#initialize empty lists and dictionary
+students_list=[]
+students_dict={}
+
+#Add student information
+name=input.append("Enter student name:")
+age=int(input("Enter student age:"))
+grade=input("Enter Student Grade:")
